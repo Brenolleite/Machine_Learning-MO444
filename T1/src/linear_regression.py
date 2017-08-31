acc9fe6ad865954e1ebf2add7801e6285bb00259
Breno Leite <brenolleite@gmail.com>
2017-08-31 12:16:34 -0300

    Organizing folders

--- T1/src/linear_regression.py
+++ T1/src/linear_regression.py
@@ -10,7 +10,7 @@ from sklearn.linear_model import SGDRegressor
 from sklearn.model_selection import KFold, cross_val_predict, train_test_split, learning_curve
 
 # Read training data
-train_file = np.loadtxt('year-prediction-msd-train.txt', delimiter=',')
+train_file = np.loadtxt('../dataset/year-prediction-msd-train.txt', delimiter=',')
 
 # ---- PRE-PROCESSING ----
 	# Scale data
@@ -24,16 +24,16 @@ data_norm = preprocessing.normalize(train_file, norm='l2')
 	# Divide in training and test sets
 train_labels = data_norm[:,0]
 train_data = data_norm[:,1:]
-	
+
 	# Select some features
 #train_labels = train_labels[78:]
 #train_data = train_data[78:]
 
 # ---- REGRESSION MODEL ----
 	# Create linear regression
-reg = linear_model.SGDRegressor(alpha=0.0001, average=True, n_iter=50, verbose=False)
+reg = linear_model.SGDRegressor(alpha=0.0001, average=True, n_iter_=5, verbose=False)
 #reg = linear_model.LinearRegression(n_jobs=-1) # Create linear regression
-print reg
+print(reg)
 
 	# Compute RMSE on training data
 reg.fit(train_data,train_labels)
@@ -50,30 +50,30 @@ t_rmse = np.array([])
 cv_rmse = np.array([])
 fig, ax = plt.subplots()
 for train, test in kf.split(train_data):
-	reg.fit(train_data[train], train_labels[train])
-	predictions = reg.predict(train_data[test])
-	err = predictions - train_labels[test]
-	xval_err += np.dot(err,err)
-	rmse_10cv = np.sqrt(xval_err/len(train_data))
-	#print('Coefficients:', reg.coef_) # The coefficients
-	print("Mean squared error: %.2f" % mean_squared_error(predictions, train_labels[test])) # The mean squared error
-	# Explained variance score: 1 is perfect prediction
-	r2_train = reg.score(train_data[train], train_labels[train])
-	r2_test = reg.score(train_data[test], train_labels[test])
-	print('R2 no set de treino: %.2f' % r2_train)
-	print('R2 no set de teste: %.2f' % r2_test)
-	print('Variance score: %.2f' % r2_score(predictions, train_labels[test]))
-	print 'slope = {0}'.format(reg.coef_[0])
-	print 'intercept(bias) = {0}'.format(reg.intercept_)
-	plt.scatter(train_data[test][:,0], train_labels[test],  color='red')
-	plt.plot(train_data[test][:,0], predictions, color='blue', linewidth=1)
+    reg.fit(train_data[train], train_labels[train])
+    predictions = reg.predict(train_data[test])
+    err = predictions - train_labels[test]
+    xval_err += np.dot(err,err)
+    rmse_10cv = np.sqrt(xval_err/len(train_data))
+    #print('Coefficients:', reg.coef_) # The coefficients
+    print("Mean squared error: %.2f" % mean_squared_error(predictions, train_labels[test])) # The mean squared error
+    # Explained variance score: 1 is perfect prediction
+    r2_train = reg.score(train_data[train], train_labels[train])
+    r2_test = reg.score(train_data[test], train_labels[test])
+    print('R2 no set de treino: %.2f' % r2_train)
+    print('R2 no set de teste: %.2f' % r2_test)
+    print('Variance score: %.2f' % r2_score(predictions, train_labels[test]))
+    print('slope = {0}'.format(reg.coef_[0]))
+    print('intercept(bias) = {0}'.format(reg.intercept_))
+    plt.scatter(train_data[test][:,0], train_labels[test],  color='red')
+    plt.plot(train_data[test][:,0], predictions, color='blue', linewidth=1)
 
 t_rmse = np.append(t_rmse, [rmse_train])
 cv_rmse = np.append(cv_rmse, [rmse_10cv])
 print('RMSE treino {:.4f}\t\t RMSE 10CV {:.4f}'.format(rmse_train,rmse_10cv))
 
 	# Plot Learning Curve
-train_sz, train_errs, cv_errs = learning_curve(estimator=reg, X=train_data, y=train_labels, cv=n_folds, train_sizes=np.linspace(0.05, 1, 20), scoring="neg_mean_squared_error")  
+train_sz, train_errs, cv_errs = learning_curve(estimator=reg, X=train_data, y=train_labels, cv=n_folds, train_sizes=np.linspace(0.05, 1, 20), scoring="neg_mean_squared_error")
 tr_err = np.mean(train_errs, axis=1)
 cv_err = np.mean(cv_errs, axis=1)
 plt.show()
@@ -90,7 +90,7 @@ plt.show()
 
 # ---- TEST ----
 # Reading test file
-#test_file = np.loadtxt('year-prediction-msd-test.txt', delimiter=',')
+#test_file = np.loadtxt('../dataset/year-prediction-msd-test.txt', delimiter=',')
 
 # Need to FIX
 #test_gt = test_file[:,0]
