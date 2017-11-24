66e6019864538462e46a11715153860517aed9c9
barbara <barbarabenato@gmail.com>
2017-11-24 15:00:28 -0200

    preprocessing

--- Final/main.py
+++ Final/main.py
@@ -6,7 +6,7 @@ import time
 path_train = '/home/barbara/Documents/Trabalho/train-jpg/'
 new_path_train = '/home/barbara/Documents/Trabalho/train-jpg_resized/'
 path_label = '/home/barbara/Documents/Trabalho/train_v2.csv'
-path_tsne = 'tsne'
+path_tsne = 'tsne10'
 
 #Resizing images 32x32
 #li.resize_images(path_train, new_path_train, 32)
@@ -16,8 +16,12 @@ start_time = time.time()
 data = dat.load_images(new_path_train)
 print 'Loaded in ' + str(time.time()-start_time) + 's'
 
+# Preprocessing
 data = data.astype('float32')
 data /= 255.  
+data = pp.st_scale(data)
+data = pp.normalize_l2(data)
+data, i = pp.PCA_reduction(data, 0, 10)
 
 #Loading labels
 label = dat.load_labels(path_label)
