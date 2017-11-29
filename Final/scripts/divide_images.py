cf870c457dbe26f93b56290508cbb7f2f952699f
Breno Leite <brenolleite@gmail.com>
2017-11-29 21:11:21 -0200

    Adding type to script

--- Final/scripts/divide_images.py
+++ Final/scripts/divide_images.py
@@ -71,29 +71,58 @@ predicts = {}
 for i in range(len(f)):
     predicts[f[i][0]] = f[i][1].split(' ')
 
-# Go over all predictions
-for pred in predicts.items():
-    # Get labels for file
-    ground_truth = labels[pred[0]]
+# Verify type of script
+# 1 - Look at predictions
+# 2 - Look at truth labels
+if ARGS.type == 1 or ARGS.type == None:
+    # Go over all predictions
+    for pred in predicts.items():
+        # Get labels for file
+        ground_truth = labels[pred[0]]
 
-    # Create image with labels
-    path = ARGS.dataset_path + '/' + pred[0] + '.jpg'
-    img = create_image(path, ground_truth, pred[1])
+        # Create image with labels
+        path = ARGS.dataset_path + '/' + pred[0] + '.jpg'
+        img = create_image(path, ground_truth, pred[1])
 
-    # Go over gt labels comparing each class
-    for label in ground_truth:
-        # Get path to write output
-        path = ARGS.output_path
+        # Go over gt labels comparing each class
+        for label in ground_truth:
+            # Get path to write output
+            path = ARGS.output_path
 
-        # Verify label
-        if label in pred[1]:
-            path += '/correct/'
-        else:
-            path += '/wrong/'
+            # Verify label
+            if label in pred[1]:
+                path += '/correct/'
+            else:
+                path += '/wrong/'
 
-        path += label + '/' + pred[0]
+            path += label + '/' + pred[0]
 
-        # Save image in the path
-        save_image(path, img)
+            # Save image in the path
+            save_image(path, img)
+# Code for looking at the truth labels
+# Copy and paste for better readability
+else:
+    # Go over all predictions
+    for pred in predicts.items():
+        # Get labels for file
+        ground_truth = labels[pred[0]]
 
-        #save_image(path, img)
+        # Create image with labels
+        path = ARGS.dataset_path + '/' + pred[0] + '.jpg'
+        img = create_image(path, ground_truth, pred[1])
+
+        # Go over predictions classes comparing to labels
+        for c in pred[1]:
+            # Get path to write output
+            path = ARGS.output_path
+
+            # Verify predicion
+            if c in ground_truth:
+                path += '/correct/'
+            else:
+                path += '/wrong/'
+
+            path += c + '/' + pred[0]
+
+            # Save image in the path
+            save_image(path, img)
